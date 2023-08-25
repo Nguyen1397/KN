45f7e3159466750f6c86a862b5d11dfc778f69ee
Nguyen1397 <142870419+Nguyen1397@users.noreply.github.com>
2023-08-25 10:19:17 +0700

    Update tinhsum.c

--- New folder/tinhsum.c
+++ New folder/tinhsum.c
@@ -3,10 +3,12 @@ int main(){
     double T, V, A, Sum;
     printf("Nhap diem toan, van, anh: ");
     scanf("%lf %lf %lf", &T, &V, &A);
-    if(T < 0 || T > 10 || V < 0 || V > 10 || A < 0 || A > 10){ //diem 1 mon bat ki ngoai khoang [0,10] -> ko hop le
+
+//diem 1 mon bat ki ngoai khoang [0,10] -> ko hop le
+    if(T < 0 || T > 10 || V < 0 || V > 10 || A < 0 || A > 10){ 
     	printf("Diem khong hop le");
-	}
-	
+    }
+
 	else{
         Sum = 2*T + 2*V + A;
         if(Sum >= 40){
@@ -15,6 +17,5 @@ int main(){
             printf("Fail");
         }
     }
-
     return 0;
 }
