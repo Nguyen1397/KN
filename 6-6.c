04147447781ac7921a864985c3f13fad9fd89f79
Nguyen1397 <knguyen4582@gmail.com>
2023-08-28 06:10:55 +0700

    del

--- 6-6.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-}
--- 6-9.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    char line[80]; /* line[80] la 1 mang luu 80 ki tu*/
-    scanf("%[ ABCDEFGHIJKLMNOPRSTUVWXYZ]", line);
-}
