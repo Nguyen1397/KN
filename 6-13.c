413559974d0986a97d6cada681c20fdec602d83d
Nguyen1397 <142870419+Nguyen1397@users.noreply.github.com>
2023-08-28 06:08:52 +0700

    Delete 6-13.c

--- 6-13.c
+++
@@ -1,9 +0,0 @@
-/* Chuong trinh trinh bay getchar() va putchar() */
-#include <stdio.h>
-void main()
-{
-    char letter;
-    printf("You can enter a chatracter now: ");
-    letter = getchar();
-    putchar(letter);
-}
