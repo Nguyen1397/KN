495be512cb5404b87e10c7ff999899edaf5b6c79
Nguyen1397 <knguyen4582@gmail.com>
2023-09-18 09:56:52 +0700

    Create menu.c

---
+++ Bai-tren-lop/menu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void Menu();
+void main()
+{
+    printf("Toi muon xem Menu cua quan !");
+    Menu();
+}
+
+void Menu()
+{
+    printf("\nCom rang dua bo     : 30k");
+    printf("\nCom rang thap cam   : 20k");
+    printf("\nCom rang xa xiu     : 25k");
+    printf("\nCom suat            : 25k");
+    printf("\nCoca cola           : 10k");
+    printf("\nNuoc cam lon        : 10k");
+    printf("\nTra chanh           : 15k");
+    printf("\nTra da              : 3k");
+}
